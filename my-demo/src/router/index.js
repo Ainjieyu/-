bc4c2181d4b0cb49ed2e5d07dbd4cacc173cf94d
ligen <1165028742@qq.com>
2022-10-20 19:56:13 +0800

    20221020

--- my-demo/src/router/index.js
+++ my-demo/src/router/index.js
@@ -1,4 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import User from '../views/User.vue'
+
+const routes = [
+    {path:'/home',component:Home},
+    {path:'/user',component:User},
+]
 
 Vue.use(VueRouter)
