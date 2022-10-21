5060d97628efa802eb05765a2f18ce164ba971a8
ligen <1165028742@qq.com>
2022-10-21 19:50:39 +0800

    1021

--- my-demo/src/router/index.js
+++ my-demo/src/router/index.js
@@ -2,10 +2,15 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import User from '../views/User.vue'
+Vue.use(VueRouter)
 
 const routes = [
     {path:'/home',component:Home},
     {path:'/user',component:User},
 ]
 
-Vue.use(VueRouter)
+const router = new VueRouter({
+    routes
+})
+export default router
+
