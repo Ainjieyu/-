50364329558c9e239efb8eba37a1ef2f79b9d98c
ligen <1165028742@qq.com>
2022-10-31 20:09:30 +0800

    1031

--- my-demo/src/views/Home.vue
+++ my-demo/src/views/Home.vue
@@ -3,7 +3,7 @@
     <el-col :span="8">
       <el-card class="box-card">
         <div class="user">
-          <img src="../assets/images/user.png" alt="">
+          <img src="../assets/images/user.png" alt="" />
           <div class="user-info">
             <p class="name">admin</p>
             <p class="access">管理员</p>
@@ -14,6 +14,15 @@
           <p>上次登录地点: <span>上海</span></p>
         </div>
       </el-card>
+      <el-card>
+        <el-table :data="tableData" style="width: 100%">
+          <el-table-column
+            v-for="(val, key) in tableLabel"
+            :prop="key"
+            :label="val"
+          />
+        </el-table>
+      </el-card>
     </el-col>
     <el-col :span="16"><div class="grid-content bg-purple-light"></div></el-col>
   </el-row>
@@ -21,40 +30,88 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      tableData: [
+        {
+          name: "oppo",
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800,
+        },
+        {
+          name: "vivo",
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800,
+        },
+        {
+          name: "苹果",
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800,
+        },
+        {
+          name: "小米",
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800,
+        },
+        {
+          name: "三星",
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800,
+        },
+        {
+          name: "魅族",
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800,
+        },
+      ],
+      tableLabel: {
+        name: "课程",
+        todayBuy: "今日购买",
+        monthBuy: "本月购买",
+        totalBuy: "总销量",
+      },
+    };
   },
 };
 </script>
 <style lang="less" scoped>
-.user{
+el-card {
+  margin-top: 20px;
+  height: 460px;
+}
+.user {
   display: flex;
   align-items: center;
   padding-bottom: 20px;
   margin-bottom: 20px;
   border-bottom: 1px solid #ccc;
-  img{
+  img {
     margin-right: 40px;
     width: 150px;
     height: 150px;
     border-radius: 50%;
   }
 }
-.user-info{
-  
-  .name{
+.user-info {
+  .name {
     font-size: 32px;
     margin-bottom: 10px;
   }
-  .access{
-    color:#999;
+  .access {
+    color: #999;
   }
 }
-.login-info{
-  p{
+.login-info {
+  p {
     line-height: 28px;
     font-size: 14px;
     color: #999;
-    span{
+    span {
       margin-left: 60px;
       color: #666;
     }
